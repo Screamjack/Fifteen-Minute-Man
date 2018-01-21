a80636274ec17437cb6bed31ac63a6a9b4539792
Noah <scr3amjack@gmail.com>
2018-01-21 17:22:04 -0500

    Started Trigger Stuff

---
+++ 3DTesting/Assets/Scripts/Triggers/TestTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTrigger : MonoBehaviour, TriggerInterface {
+
+    [SerializeField]
+    Transform block;
+
+    bool triggerStarted = false;
+    bool triggerDone = false;
+
+    public bool completed
+    {
+        get { return triggerDone; }
+    }
+
+    [SerializeField]
+    List<TriggerInterface> preReqs;
+
+    public bool CheckTrigger()
+    {
+        bool retVal = true;
+        foreach(TriggerInterface t in preReqs)
+        {
+            if (!t.completed)
+                retVal = false;
+        }
+        return retVal;
+    }
+
+    public void ActivateTrigger()
+    {
+        triggerStarted = true;
+        StartCoroutine(LiftBlock(1));
+    }
+
+    IEnumerator LiftBlock(float duration)
+    {
+        for (float i = 0; i < duration; i += Time.deltaTime)
+        {
+            block.transform.position += Vector3.up * 0.01f;
+            yield return null;
+        }
+        triggerDone = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!triggerStarted && !triggerDone)
+        {
+            ActivateTrigger();
+        }
+    }
+}
---
+++ 3DTesting/Assets/Scripts/Triggers/TriggerInterface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface TriggerInterface {
+
+    /// <summary>
+    /// Has this trigger been completed? 
+    /// </summary>
+    bool completed
+    {
+        get;
+    }
+
+    /// <summary>
+    /// The actual method for applying the trigger.
+    /// </summary>
+    void ActivateTrigger();
+
+    /// <summary>
+    /// A means of checking whether or not the trigger can be activated. 
+    /// </summary>
+    /// <returns>True if the trigger can be activated</returns>
+    bool CheckTrigger(); 
+
+
+}
